595227f83c33a5d26e39f00795de8f95afb23148
Jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-03-31 11:50:32 +0200

    removed unused variables

--- R/which_rows_with_no_sd.R
+++ R/which_rows_with_no_sd.R
@@ -9,8 +9,15 @@
 #' @examples RNAseq_Annotated_Matrix<-Create_Rank_Columns(RNAseq_Annotated_Matrix)
 
 which_rows_with_no_sd <- function(RNAseq_Annotated_Matrix){
+  SS<-2
+  SE<-length(sample_names)+1
   sd_of_zero<-NULL
-  for (i in 1:dim(RNAseq_Annotated_Matrix)[1]) {if(sd(RNAseq_Annotated_Matrix[i,SS:SE])==0) {sd_of_zero<-c(sd_of_zero,i)}}
+
+  for (i in 1:dim(RNAseq_Annotated_Matrix)[1]) {
+    if(sd(RNAseq_Annotated_Matrix[i,SS:SE])==0) {
+      sd_of_zero<-c(sd_of_zero,i)
+    }
+  }
   RNAseq_Annotation_Matrix_no_sd_of_zero<-RNAseq_Annotated_Matrix[-sd_of_zero,]
   return(RNAseq_Annotation_Matrix_no_sd_of_zero)
 }
