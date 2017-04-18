93c7a5d140c8ea0a10cbfcd9ca48a14bab9155e5
Jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-04-18 10:20:37 +0200

    added documentation

--- src/which_rows_with_no_sd.cpp
+++ src/which_rows_with_no_sd.cpp
@@ -5,12 +5,29 @@
 using namespace Rcpp;
 // [[Rcpp::plugins(cpp11)]]
 
+/**
+ * Round a floating point number by adding or substracting 0.5 before rounding.
+ *
+ * @param val A double containing the value
+ *
+ * @author Joris van Steenbrugge
+ * @version 1.0 13 april 2017
+ */
 inline double round( double val )
 {
   if( val < 0 ) return ceil(val - 0.5);
   return floor(val + 0.5);
 }
 
+/**
+ * Calculate the mean of an array with doubles
+ *
+ * @param values A pointer to an array of doubles
+ * @param size The length of the array
+ *
+ * @author Joris van Steenbrugge
+ * @version 1.0 10 april 2017
+ */
 double CalculateMean(double * values, int size)
 {
     double sum = 0;
@@ -21,6 +38,16 @@ double CalculateMean(double * values, int size)
     return (sum / size);
 }
 
+/**
+ * Generic function to calculate the sample variance
+ *
+ * @param values A pointer to an array of doubles
+ * @param mean The mean value of the array with doubles
+ * @param size The lenght of the array
+ *
+ * @author Joris van Steenbrugge
+ * @version 1.0 10 april 2017
+ */
 double CalculateVariance(double * values, double mean, int size)
 {
     double temp = 0;
@@ -32,6 +59,16 @@ double CalculateVariance(double * values, double mean, int size)
     return temp / (size - 1 );
 }
 
+/**
+ * Generic function to calculate the standard deviation as the square root of
+ * the sample variance.
+ *
+ * @param values A pointer to an array of doubles
+ * @param size The length of the array
+ *
+ * @author Joris van Steenbrugge
+ * @version 1.0 10 april 2017
+ */
 double Calculate_StandardDeviation(double * values, int size)
 {
     double mean = CalculateMean(values, size);
