81e169d9c38ffbb39486b6711e5aca46df7588d1
Jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-04-20 09:05:30 +0200

    improvements wip

--- R/Create_Rank_Columns.R
+++ R/Create_Rank_Columns.R
@@ -24,8 +24,40 @@ Create_Rank_Columns <- function(RNAseq_Annotated_Matrix, matrix_features){
     for (s in 1:length(matrix_features@high_quality_bins)) {
       matrix_HQ_bins <- RNAseq_Annotated_Matrix[which(RNAseq_Annotated_Matrix$Bin==matrix_features@high_quality_bins[s]),]
       matrix_HQ_bins[,matrix_features@Bin_Column+i]<-rank(-matrix_HQ_bins[,i+1], na.last=TRUE, ties.method="random")/max(rank(-matrix_HQ_bins[,i+1], na.last=TRUE, ties.method="random"))
-      RNAseq_Annotated_Matrix[which(RNAseq_Annotated_Matrix$Bin==high_quality_bins[s]),] <- matrix_HQ_bins    }
+      RNAseq_Annotated_Matrix[which(RNAseq_Annotated_Matrix$Bin==high_quality_bins[s]),] <- matrix_HQ_bins
+    }
 
   }
   return(RNAseq_Annotated_Matrix)
+}
+
+
+Create_Rank_Columns2 <- function(RNAseq_Annotated_Matrix, matrix_features){
+
+
+  for (i in 1:matrix_features@sample_size) {
+    Rank_column_name<-paste("rank",i,sep="")
+    RNAseq_Annotated_Matrix[,matrix_features@Bin_Column+i]<-c(NA)
+  }
+
+  #  colnames(RNAseq_Annotated_Matrix)<-c("Locus_ID",matrix_features@sample_names,"KO","Bin",Rank_column_name)
+
+  # add to the matrix the ranks
+  # for (i in 1:matrix_features@sample_size) {
+  #   for (s in 1:length(matrix_features@high_quality_bins)) {
+  #     matrix_HQ_bins <- RNAseq_Annotated_Matrix[which(RNAseq_Annotated_Matrix$Bin==matrix_features@high_quality_bins[s]),]
+  #     ranks <- rank(-matrix_HQ_bins[,i+1], na.last=TRUE, ties.method="random")
+  #     matrix_HQ_bins[,matrix_features@Bin_Column+i]<-ranks/max(ranks)
+  #     RNAseq_Annotated_Matrix[which(RNAseq_Annotated_Matrix$Bin==high_quality_bins[s]),] <- matrix_HQ_bins
+  #   }
+  #
+  # }
+
+  sample_cols <- matrix_features@SS:matrix_features@SE
+  data_matrix <- matrix(apply(RNAseq_Annotated_Matrix[, sample_cols], 2, as.numeric), ncol = length(sample_cols))
+  vals <- GetRanksPerBin(data_matrix,
+                 sapply(RNAseq_Annotated_Matrix[, matrix_features@Bin_Column], as.numeric),
+                 matrix_features@high_quality_bins)
+
+  return(vals)
 }
