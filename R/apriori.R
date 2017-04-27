d581708c2773611c0979aa8c09939837bd7b3359
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-04-27 23:19:19 +0200

    wip

--- R/apriori.R
+++ R/apriori.R
@@ -1,9 +1,13 @@
+#apriori
 library(arules)
 library(arulesViz)
+#rock
+library(cba)
 
 Apriori <- setRefClass("AssociationRules",
-                       fields = list(dataset = "matrix"),
-                       methods = list(
+                       fields = list(dataset = "data.frame",
+                                     flatdata = "matrix"),
+                         methods = list(
                          run = function(){
                           return(apriori(dataset, parameter=list(support=0.3, confidence=0.5)))},
                          plot_graph = function(rules, n = 50 ){
@@ -11,35 +15,45 @@ Apriori <- setRefClass("AssociationRules",
                          get_top10 = function(rules){
                            return(inspect(head(sort(rules), n=10)))},
                          Data_clean = function(){
-                           dataset <<- apply(dataset, 2, as.factor)
-                           extendCollumns(dataset[,2])
 
+                            extendCollumns = function(column, starting_matrix){
+                                cluster_to_true_false = function(val, clusternum){
+                                  if(is.na(val) || val != clusternum){
+                                    return(0)
+                                  }else{
+                                    return(1)
+                                  }
+                                }
+                                
+                              
+                              for(i in levels(as.factor(column))){
+                                vec <- column
+                                starting_matrix <- cbind(starting_matrix, sapply(vec, cluster_to_true_false, clusternum=i))
+                              }
+                              return(starting_matrix)
+                            }
+                           starting_matrix <- matrix(nrow = nrow(dataset))
+                           for(i in ncol(dataset)){
+                             starting_matrix <- extendCollumns(dataset[,i], starting_matrix)
+                           }
+                           #starting_matrix <- starting_matrix[,2: ncol(starting_matrix)]
+                           print(starting_matrix)
 
                          }
                        )
 )
-extendCollumns = function(column){
-    print(levels(column))
-}
 
 
-test <- Apriori$new(dataset = c1_data)
-rules <- test$run()
-test$plot_graph(rules)
-test$get_top10(rules)
 
 
 test <- Apriori$new(dataset = dat)
 test$Data_clean()
-
-
-
-
+levels(as.factor(test$cleandata[,2]))
 
 
 #load a table
-dat <- as.matrix(read.csv("/home/joris/Desktop/module_cluster_matrix.csv"))
-rownames(dat) <- dat$Bins
+dat <- read.csv("/home/joris/Downloads/module_cluster_matrix2.csv")
+rownames(dat) <- dat[,1]
 dat$Bins <- NULL
 
 
