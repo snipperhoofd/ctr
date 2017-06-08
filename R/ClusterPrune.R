124be2c8f75204e0c6e5e3382318e3b48fa671ce
Oyserman <Oyserman@users.noreply.github.com>
2017-06-08 17:08:41 +0200

    Create ClusterPrune.R

--- R/ClusterPrune.R
+++ R/ClusterPrune.R
@@ -10,6 +10,17 @@
 #' @examples PHA_pvalues_BR<-ClusterPrune(PHA_clustering_results_P_NRED_BR,Random_Background_Module_Distances_6_BR)
 
 
+# Psuedo code for how this function should work
+# 1) identify the N in the module being tested and get the correct background distribution for that 'N'
+# 2) identify the bins in a cluster (so each module will have multiple clusters)
+# 3) get the Z-scores of all the pairwise comparisons in this cluster
+# 4) t.test (one-sided) to determine if the Z-scores in the cluster is significanlty 
+#     less than expected based on the background distribution
+# 5) if it is significant, keep it
+# 6) Bonus Round, if it is not significant, drop the lowest scoring genome from the comparison and re-test 
+# (repeat until significant or <3 genomes)
+
+
 ClusterPrune <- function(clustering_results,Background_Module_Distances) {
   pvalues<-rep(NA,length(clustering_results$cl))
   distances<-list(NA)
