916978a73b8846124f9fff38da061a6c87421f07
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-06-16 16:26:06 +0200

    working cluster -> pruning -> asso_matrix

--- R/fill_ass_matrix.R
+++ R/fill_ass_matrix.R
@@ -10,29 +10,40 @@
 #' @examples M_I_association_matrix<-fill_association_matrix(M_I_clustering_results_P_NRED,matrix_features_BR,names(M_I_module_list))
 
 
-fill_association_matrix<-function(clustering_results, matrix_features, module_names) {
+fill_association_matrix <- function(clustering_results, matrix_features,
+                                    module_names) {
 
-  association_matrix<-matrix(data = NA,
+  association_matrix <- matrix(data = NA,
                              nrow = length(matrix_features@high_quality_bins),
                              ncol = length(clustering_results),
                              dimnames = list(matrix_features@high_quality_bins,
-                                             module_names))
+                                             names(module_names)))
+
 
   for (i in 1: length(clustering_results)) {
 
-    if (length((clustering_results[[i]]$cl$membership)) != 0) {
+    current_cluster <- clustering_results[[i]]$cl
 
-      for (j in 1: length(clustering_results[[i]]$cl)) {
+    unique_members <- unique(current_cluster$membership)
+    if (length(unique_members) != 0) {
 
-        for (k in 1: length(clustering_results[[i]]$cl[[j]])) {
-          bin <- as.numeric(clustering_results[[i]]$cl[[j]][k])
-          row <- which(matrix_features@high_quality_bins == bin)
+      for (j in 1: length(unique_members)) {
+          #Best variable name incoming
+          cluster_cluster <-  current_cluster[[j]]
 
-          association_matrix[row,i] <- j
-        }
+          for (k in 1: length(current_cluster[[j]])) {
+            bin <- as.numeric(current_cluster[[j]][k])
+            row <- which(matrix_features@high_quality_bins == bin)
+
+            association_matrix[row,i] <- j
+          }
 
       }
+
     }
-  }
+
+    }
   return(association_matrix)
 }
+
+
